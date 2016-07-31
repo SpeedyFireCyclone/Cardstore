1091ff7f98d3c6d6c340693bce35471d4a535d5d
SpeedyFireCyclone <remcohofman98@gmail.com>
2016-07-31 14:36:24 +0200

    Added open-source licenses activity in app.

--- app/src/main/java/com/speedyfirecyclone/cardstore/MainActivity.java
+++ app/src/main/java/com/speedyfirecyclone/cardstore/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -18,7 +21,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -78,6 +80,25 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.overflowFOSS:
+                Intent FOSS = new Intent(this, Licenses.class);
+                startActivity(FOSS);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void createCard() {
